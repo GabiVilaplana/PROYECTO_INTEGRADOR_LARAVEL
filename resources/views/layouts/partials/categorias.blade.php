5a90ba13caf061568e92a93913c57f572e11230d
Sitoosito8 <a20000lex@gmail.com>
2026-02-03 20:15:53 +0100

    Subir cambios relacionados con las los servicios y las categorias

--- resources/views/layouts/partials/categorias.blade.php
+++ resources/views/layouts/partials/categorias.blade.php
@@ -3,7 +3,6 @@
 <div class="carousel-wrapper">
     <h2>Categorias</h2>
     <button class="carousel-btn left" id="btn-left">‹</button>
-    <button class="carousel-btn right" id="btn-right">›</button>
     <section class="courses carousel-track" id="datosCategoria">
         @foreach($categorias as $categoria)
             <div class="card">
@@ -14,4 +13,5 @@
         @endforeach
 
     </section>
+    <button class="carousel-btn right" id="btn-right">›</button>
 </div>
