a1e26e15aff2c6814dbae22f94e582ddb0d8f6ef
Gabi Vilaplana <gabired345@gmail.com>
2026-02-18 18:22:22 +0100

    Subir cambios relacionados  con el has de usuarios

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Usuario extends Authenticatable
 {
